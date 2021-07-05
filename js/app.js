75bedb52730b574ea2f44932d25faa44df690126
unknown <syedrakesh.me@gmail.com>
2021-07-06 04:05:58 +0600

    Refactoring Code: The DRY Principle

--- js/app.js
+++ js/app.js
@@ -1,40 +1,45 @@
 'use strict';
 
-const gameRange = 20;
+const gameRange = 20; // Game Range Between 1 - 20
 
-let secretNumber = Math.trunc(Math.random() * gameRange ) + 1;
-//document.querySelector('.secretNumber').textContent = secretNumber;
-document.querySelector('.secretNumber').textContent = '?';
+function displayMessage(message){ // DRY_1 - Don't Repeat yourself
+	document.querySelector('.message').textContent = message;
+}
+
+let secretNumber = Math.trunc(Math.random() * gameRange ) + 1; // Generate Random Number
+//document.querySelector('.secretNumber').textContent = secretNumber; // Display Secret Number
 
 let score = gameRange;
-document.querySelector('.score').textContent = score;
+document.querySelector('.score').textContent = score; // Display Score
 
-let highscore = 0;
-document.querySelector('.highscore').textContent = highscore;
+let highScore = 0;
+document.querySelector('.highScore').textContent = highScore; // Display High Score
 
+// Retry & Play Again Button
 document.querySelector('.retry').addEventListener('click', function(){
 	score = gameRange;
 	document.querySelector('.score').textContent = gameRange;
 	
 	secretNumber = Math.trunc(Math.random() * gameRange ) + 1;
 	
-	document.querySelector('.message').textContent = 'Start guessing...';
+	//document.querySelector('.message').textContent = 'Start guessing...';
+	displayMessage('Start guessing...'); // DRY_1 - Don't Repeat yourself
 	
 	document.querySelector('.guessNumber').value = ' ';
 	
-	document.querySelector('.secretNumber').textContent = '?';
-	//document.querySelector('.secretNumber').textContent = secretNumber;
+	
 	
 	document.querySelector('.container').style.backgroundColor = '#D1FAE5';
 	
 	document.querySelector('.secretNumber').style.backgroundColor = '#6EE7B7';
 	document.querySelector('.secretNumber').style.width = '5rem';
+	document.querySelector('.secretNumber').textContent = '?';
+	//document.querySelector('.secretNumber').textContent = secretNumber;
 	
 	document.querySelector('.retry').style.backgroundColor = '#A7F3D0';
 	document.querySelector('.retry').textContent = 'Retry!';
 	
 	document.querySelector('.check').style.backgroundColor = '#A7F3D0';
-	document.querySelector('.check').style.color = '#000000';
 	document.querySelector('.check').addEventListener('mouseenter', function() {
 		document.querySelector('.check').style.color = '#ECFDF5';
 	});
@@ -50,13 +55,19 @@ document.querySelector('.retry').addEventListener('click', function(){
 	
 });
 
+// Check Guessing Number
 document.querySelector('.check').addEventListener('click', function () {
     const guessNumber = Number(document.querySelector('.guessNumber').value);
 
+	// If No Input
     if (!guessNumber) {
-        document.querySelector('.message').textContent = '❓ No number!';
+        //document.querySelector('.message').textContent = '❓ No number!';
+		displayMessage('❓ No number!'); // DRY_1 - Don't Repeat yourself
+		
+	// If Match The Input
     } else if (guessNumber === secretNumber) {
-        document.querySelector('.message').textContent = '✅ Correct Number!';
+        //document.querySelector('.message').textContent = '✅ Correct Number!';
+		displayMessage('✅ Correct Number!'); // DRY_1 - Don't Repeat yourself
 		
 		document.querySelector('.secretNumber').textContent = secretNumber;
 		
@@ -76,12 +87,29 @@ document.querySelector('.check').addEventListener('click', function () {
 		document.querySelector('.guessNumber').style.cursor = 'not-allowed';
 		document.querySelector('.guessNumber').disabled = true;
 		
-		if ( score > highscore ){
-			highscore = score;
-			document.querySelector('.highscore').textContent = highscore;
+		// Update High Score
+		if ( score > highScore ){
+			highScore = score;
+			document.querySelector('.highScore').textContent = highScore;
 		}
 		
-    } else if (guessNumber > secretNumber) {
+    } else if ( guessNumber != secretNumber ) { // DRY_2- Don't Repeat yourself
+		if ( score > 1 ){
+			//document.querySelector('.message').textContent = guessNumber > secretNumber ? '🚫 Too high!': '🚫 Too low!';
+			displayMessage(guessNumber > secretNumber ? '🚫 Too high!': '🚫 Too low!'); // DRY_1 - Don't Repeat yourself
+			score--;
+			document.querySelector('.score').textContent = score;
+		}else {
+			//document.querySelector('.message').textContent = '🥶 You lose the game!';
+			displayMessage('🥶 You lose the game!'); // DRY_1 - Don't Repeat yourself
+			document.querySelector('.score').textContent = 0;
+		}
+	}
+	/*
+	
+	DRY_2 - Don't Repeat yourself
+	
+	else if (guessNumber > secretNumber) {
         if ( score > 1 ){
 			document.querySelector('.message').textContent = '🚫 Too high!';
 			score--;
@@ -101,4 +129,5 @@ document.querySelector('.check').addEventListener('click', function () {
 		}
         
     }
+	*/
 });
